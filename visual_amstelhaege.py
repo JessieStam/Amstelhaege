d5bafd811e389a98f2fcc50434db2f7b36174ba2
CLSch <caitlin.schaffers@gmail.com>
2016-04-26 12:48:57 +0200

    Revert "initial commit"
    This reverts commit 7e5120ebe196184dd92921bffc3366040276b5a1.

--- visual_amstelhaege.py
+++ visual_amstelhaege.py
@@ -1,11 +1,1 @@
 import pygame
-pygame.init()
-
-
-GREEN = (100, 200, 200)
-
-DISPLAYSURF = pygame.display.set_mode((1024, 768), 0, 32)
-DISPLAYSURF.fill(GREEN)
-pygame.draw.rect(DISPLAYSURF, GREEN, (10, 30, 150, 160))
-
-pygame.time.delay(10000)
