dda75ab34c107a1eadbd6aedbe5c7fcbe8188c79
DemiVollemans <demileev@hotmail.com>
2016-05-12 15:04:10 +0200

    grove algoritme

--- algoritme.py
+++ algoritme.py
@@ -1,36 +1,81 @@
-# pseudocode;
-# begin met een kleine grid
-# kan je de huisjes erkennen & waarde berekenen?
-# kan je de huisjes van positie laten veranderen die legitime is en waarde berekenen
-# dan algoritme toepassen, kijken of er nog een kans is dat dit algoritme aangenomen kan worden
-# exp^(current - neighboor solution)/ (value) --- hoe kleiner het verschil& hoe hoger de waarde, des te waarschijnlijker aangenomen te worden.
+def hillclimb (init_value_grid, move, max_repetitions):
+	best = value_init_grid
+	
+	repetitions = 1
 
-# values initialiseren van soorten huisjes en de waarde erbij
+	while repetitions < max_repetitions:
+		#search for next grid
+		roep new grid functie aan. 
+		calculater(new_grid) (deze returned value_new_grid
+		for next in move(best):
+			if repetitions >= max_repetitions:
+				break
 
-Small_house = 285000
-obligatory_space = 0
-extra_space_Small_house = 8550 #voor iedere nieuwe ring extra ruimte
+		value_new_grid = calculater(new_grid)
+		repetitions +=1
+		# Is this grid better?
+		if value_new grid > value_init_grid
+			best = value_new_grid
+			move_made = True
+			break 
 
-bungalow = 399000
-extra_space_bungalow = 15960
+		if not move_made:
+			break
+			print "couldn't find a better move"	
 
-maison = 610000
-extra_space_maison = 366000
+def new_grid (init_grid):
+	## pak een random huisje en verzet het in een random richting. (misschien met een kleine range)
+	# def move_house(current_state, move):
+	    """
+        Return a random position inside the room.
+        returns: a Position object.
+        """
+        random_house = pick random house and change its position random.
+        
+        # get random numbers to generate a random position in room
+        # constraints in de functie waar hij aangeroepen wordt
+        # minus 18 because every house extands 18 tiles to the right and down (16m + 2m free space)
+        # used to be 23, dont know why
+        ran_x = random.randint(0, width - 28) 
+        ran_y = random.randint(0, depth - 28)
+        ran_pos = Position(ran_x, ran_y)
+        return ran_pos
 
-water = 0 #verplciht
+        set in init_grid
+        return new_grid
 
-#erkennen van huisjes en vrije ruimte
-def analyze(self):
+ # 	pos.x, pos.y = current_state
+
+ # 	if move == 'up':
+ # 		pos.x += 1
+ # 	elif move == 'down':
+ # 		pos.x -= 1
+ # 	elif move == "right"
+ # 		pos.y += 1
+ # 	else:
+ # 		pos.y -= 1
+ # 	next_state = pos.x, pos.y	
+
+ 	return new_grid
+
+def calculater (new_grid): 
+	extra_space_Small_house = 0
+	extra_space_bungalow = 0
+	extra_space_maison = 0
+	for every rand van 4'en in de grid:
+		extra_space_Small_house++;
+	for every rand van 5'en in de grid:
+		extra_space_bungalow++;
+	for every rand van 6'en in de grid:
+		extra_space_maison++	
+
+	value_new_grid = 3435000 + (8550 * extra_space_Small_house) + (15960 * extra_space_bungalow) + (366000 * extra_space_maison);
+	return value_new_grid;	
 	
 def anneal(solution):
 	old_value_grid = value_grid(solution)
 	# of T = 1 definitely switch, 0.0 stay put, if you know the acceptance prob. the it is compared to a radom
 	# generated number between 0 and 1. if the appect. prob. is larger than the random number then you are switching 
-
-	T = 1.0
-	T_min = 0.00000001
-	alpha = 0.9
-	while T < T_min:
 		i = 1
 		while i <= 100:
 			new_value_grid = neighboor(solution)
@@ -46,14 +91,18 @@ def anneal(solution):
 	return math.exp( -abs(new_value_grid - old_value_grid)/ value?)		
 
 
-set a  random move
+def new_grid (init_grid, move);
+	def move_house(current_state, move):
+ 	pos.x, pos.y = current_state
 
-calculate the value of the grid.
-value_grid = ((28500 * Small_house) + (8550 * extra_space_Small_house)) + ((399000 * bungalow) + (15960 * extra_space_bungalow)) + ((610000 * maison) + (366000 * extra_space_maison)) 
+ 	if move == 'up':
+ 		pos.x += 1
+ 	elif move == 'down':
+ 		pos.x -= 1
+ 	elif move == "right"
+ 		pos.y += 1
+ 	else:
+ 		pos.y -= 1
+ 	next_state = pos.x, pos.y	
 
-set a new random neighboor solution
-
-calculate the cost of the new solution
-
-compare them
-
+ 	return next_state			
